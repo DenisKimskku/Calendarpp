73226058b8f4e7eb62401e1b91b5c83245e1e796
DenisKimskku <kor8821@naver.com>
2025-12-06 18:32:08 +0900

    Initial Commit

---
+++ calendar++/calendar__App.swift
@@ -0,0 +1,17 @@
+//
+//  calendar__App.swift
+//  calendar++
+//
+//  Created by den on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct calendar__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
